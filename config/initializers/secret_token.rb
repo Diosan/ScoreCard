9232695fbba76020a12048592e948c05a05df8ae
Dion Santana <diosan@gmail.com>
2013-05-23 15:01:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreCard::Application.config.secret_token = '3c2ad69b86d8b5f889c47e36bb2711f0849631e0f28fa2284c8961527df4588ad7e6003b8460b8b0021ffc2173bd99132011aa10e6c16ae721d3160e3ba3b053'
